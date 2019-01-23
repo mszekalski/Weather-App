18699e15aece9e3adeef9f4c47f788021b2356c3
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-23 18:03:31 -0500

    changed server file

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const pino = require("express-pino-logger")();
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
-
+app.use(express.static(path.join(__dirname, "client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 app.listen(3001, () =>
   console.log("Express server is running on localhost:3001")
 );
