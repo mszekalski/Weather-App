38cb827aa5b0fbb3a55ceec3a90226e5c2f13bf0
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-23 17:37:39 -0500

    added lines to node server

--- server/index.js
+++ server/index.js
@@ -6,6 +6,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(3001, () =>
   console.log("Express server is running on localhost:3001")
 );
