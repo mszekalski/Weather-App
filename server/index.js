c3eb8f2bc7d25e6a1bbf782e472bda740c71eede
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-23 18:35:47 -0500

    changed server file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,4 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-app.listen(3001, () =>
-  console.log("Express server is running on localhost:3001")
-);
+app.listen(process.env.PORT || 3001);
