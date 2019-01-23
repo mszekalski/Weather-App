01bcfc7146b0e19435b1bcdd7627ad54fcfed253
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-23 17:47:40 -0500

    changed package json

--- server/index.js
+++ server/index.js
@@ -6,14 +6,6 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
-
 app.listen(3001, () =>
   console.log("Express server is running on localhost:3001")
 );
