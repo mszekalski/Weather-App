b4e3342503c4ab5d7517e18a375c3e1494fd14e9
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-23 17:41:16 -0500

    changed server file

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 
