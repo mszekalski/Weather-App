435cef2d7893adbf8956158f94f6e184c7fa0ad5
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-23 20:16:14 -0500

    corrected something in npm deploy

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
+const path = require("path");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,19 +10,19 @@ app.use(pino);
 const port = process.env.PORT || 3001;
 
 //Static file declaration
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 //production mode
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
   //
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "build/index.html")));
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
 //build mode
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "public/index.html"));
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
 });
 
 //start server
