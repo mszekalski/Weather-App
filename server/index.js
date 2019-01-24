84d73e2d96ec4d0020b30d58a2e4a0c5b2656d2d
Matthew Szekalski <mattszekalski@icloud.com>
2019-01-24 11:22:07 -0500

    adjusted server file

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ app.use(pino);
 
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname + "/../build")));
 
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname + "/../build/index.html"));
 });
 
 //start server
